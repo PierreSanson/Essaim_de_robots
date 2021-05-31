36cfdca1e1422a2c34a297bebdcbb808c389bee8
Pierre Sanson <pierre.sanson@student-cs.fr>
2021-05-31 18:29:54 +0200

    petite modification de combineResults.py

--- code_2A/noGUI/results/combineResults.py
+++ code_2A/noGUI/results/combineResults.py
@@ -33,4 +33,4 @@ def combineAllResultsAfterSim(nb_cores, file_names, file_path_results = "results
             writer.writerow([data_list[i] for data_list in list(data.values())] + [filename.split("\\")[-1]])
 
 
-combineAllResultsAfterSim(16, [], file_path_results="./", file_path="./", multithreaded=True, delete=False)
+combineAllResultsAfterSim(int(input("Combien de threads ? ")), [], file_path_results="./", file_path="./", multithreaded=True, delete=False)
